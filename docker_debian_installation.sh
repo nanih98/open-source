808d0661736d0e4d23e2dbf2d9a31c97e94c2708
nanih98 <49982093+nanih98@users.noreply.github.com>
2019-08-12 12:58:26 +0200

    Create docker_debian_installation.sh

---
+++ docker_debian_installation.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#Docker installation Debian based 
+sudo apt-get remove docker docker-engine docker.io containerd runc -y 
+sudo apt-get update -y 
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y 
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - 
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y 
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
