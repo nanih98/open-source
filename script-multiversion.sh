42b55ed15f143d54dc0226b61cca9129e92df0d8
nanih98 <49982093+nanih98@users.noreply.github.com>
2019-09-20 13:40:18 +0200

    Create script-multiversion.sh

---
+++ script-multiversion.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Important, quit sudo command when you are executing instructions if YOU ARE USING DOCKER. "sudo: command not found"
+if [[ -r /etc/os-release ]]; then
+    . /etc/os-release
+    echo "Running $NAME $VERSION" 
+
+    if [[ $ID = *"ubuntu" || $ID = *"debian" ]]; then
+    sleep 2 
+    sudo apt-get update   
+     
+       elif [ $ID = *"centos" ]; then
+        sleep 2 
+        sudo yum update 
+        
+            elif [ $ID = *"fedora" ]; then
+            sleep 2 
+            sudo dnf update 
+
+                elif [[ $ID = "opensuse-tumbleweed"  || $ID = "opensuse-leap" ]]; then
+                sleep 2
+                sudo zypper update 
+    fi
+else
+    echo "Not running a distribution with /etc/os-release available"
+fi
