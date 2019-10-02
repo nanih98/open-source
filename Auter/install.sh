2a8f2fa76b199e3309dbb1fb608e4073dad5085a
nanih98 <49982093+nanih98@users.noreply.github.com>
2019-10-02 10:35:32 +0200

    Update install.sh

--- Auter/install.sh
+++ Auter/install.sh
@@ -1,15 +1,17 @@
 #Script to install and setup auter
+
 #Description of auter
+
 # Auter provides automatic updates for Red Hat Enterprise Linux, CentOS, and Fedora Linux servers. Auter provides flexible scheduling to ensure that updates and reboots happen when you want them to. 
+
 #It enables you to customize how updates run: you can download updates before you can apply them, 
+
 #and you can run custom scripts before and after the updates. Auter is a more flexible option than yum-cron or dnf-automatic. 
 
 #Install steps. I think it's also for debian based systems. Try it. 
 
 sudo yum install auter -y || apt-get install auter -y || dnf install auter -y 
-
 # Enable auter
-
 sudo auter --enable
 
 #Configure auter. I use my default config from my repo.
@@ -17,16 +19,13 @@ sudo auter --enable
 sudo yum install wget -y || apt-get install wget -y 
 
 #Download code and copy it.
-sudo cd /tmp && wget https://raw.githubusercontent.com/nanih98/open-source/master/Auter/auter.conf && cp -ar auter.conf /etc/auter/
 
+cd /tmp && wget https://raw.githubusercontent.com/nanih98/open-source/master/Auter/auter.conf && sudo cp -ar auter.conf /etc/auter/
 
 #Download the cron configuration for auter. Configure it as you need it. I have it on for me to do these taread during non-production hours (at night). It is set to reset itself.
 
-sudo cd /tmp && wget https://raw.githubusercontent.com/nanih98/open-source/master/Auter/auter && cp -ar auter /etc/cron.d
+cd /tmp && wget https://raw.githubusercontent.com/nanih98/open-source/master/Auter/auter && sudo cp -ar auter /etc/cron.d
 
 #Remove the files from /tmp , there are not necessary.
-sudo cd /tmp && rm -rvf auter auter.conf  
-
-
-
+cd /tmp && rm -rvf auter auter.conf
 
