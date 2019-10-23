3ea9730a71e5d2df8ef3238131d517b8592a1497
nanih98 <49982093+nanih98@users.noreply.github.com>
2019-10-23 20:50:41 +0200

    Update docker_debian_installation.sh

--- docker_debian_installation.sh
+++ docker_debian_installation.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 #Docker installation Debian based 
+echo "Install docker on debian. Do it with root preferably"
 sudo apt-get remove docker docker-engine docker.io containerd runc -y 
 sudo apt-get update -y 
 sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y 
@@ -9,7 +10,12 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update -y 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y 
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+#Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+# for problems with root path 
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose 
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
